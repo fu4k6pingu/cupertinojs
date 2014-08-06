d0d0d7b75b2db8c65aa83e6c090d3abd2e6ea709
Jerry Marino <i@jerrymarino.com>
2014-08-05 21:39:17 -0700

    Add target for static lib

---
+++ objcjs-runtime-ios/objcjs_runtime_ios.h
@@ -0,0 +1,13 @@
+//
+//  objcjs_runtime_ios.h
+//  objcjs-runtime-ios
+//
+//  Created by Jerry Marino on 8/5/14.
+//  Copyright (c) 2014 Jerry Marino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface objcjs_runtime_ios : NSObject
+
+@end
---
+++ objcjs-runtime-ios/objcjs_runtime_ios.m
@@ -0,0 +1,13 @@
+//
+//  objcjs_runtime_ios.m
+//  objcjs-runtime-ios
+//
+//  Created by Jerry Marino on 8/5/14.
+//  Copyright (c) 2014 Jerry Marino. All rights reserved.
+//
+
+#import "objcjs_runtime_ios.h"
+
+@implementation objcjs_runtime_ios
+
+@end
