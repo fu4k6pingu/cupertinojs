5e9fe208c733c6110d889c9c93f028960c3f13cf
Jerry Marino <i@jerrymarino.com>
2014-08-05 21:22:56 -0700

    Add example app

---
+++ examples/Todoie/Todoie/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Todoie
+//
+//  Created by Jerry Marino on 8/5/14.
+//  Copyright (c) 2014 Jerry Marino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ examples/Todoie/TodoieTests/TodoieTests.m
@@ -0,0 +1,34 @@
+//
+//  TodoieTests.m
+//  TodoieTests
+//
+//  Created by Jerry Marino on 8/5/14.
+//  Copyright (c) 2014 Jerry Marino. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TodoieTests : XCTestCase
+
+@end
+
+@implementation TodoieTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
