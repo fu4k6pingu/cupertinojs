b53310418966b361d55a1232973a2614305c5c8b
Jerry Marino <i@jerrymarino.com>
2014-08-07 23:09:51 -0700

    Document and clenaup example

--- examples/Todoie/Todoie/todoie.js
+++ examples/Todoie/Todoie/todoie.js
@@ -1,5 +1,25 @@
+// objc_import - a macro similar to #import which
+// can import Objective-C headers with search path being SRC_ROOT
+//
+// Classes and selectors are exported to the global namespace
 objc_import("imports.h")
 
+// Similar to swift - Objective C selectors are converted
+// ex +[NSNumber numberWithInt:int]
+// is the same as NSNumber.numberWithInt(int)
+// it is also possible to invoke selectors with
+// thier objc name ex NSNumber["numberWithInt:"](int)
+function Number(int){
+    return NSNumber.numberWithInt(int)
+}
+
+//JS numbers are implemented as NSNumbers
+function ObjCInt(int){
+    return int.intValue
+}
+
+// Structs are wrapped in JS Objects and can be accessed
+// with category methods ex -UIView.initWithFrameValue
 function Rect(x, y, width, height){
     return {
         "x" : x,
@@ -9,65 +29,76 @@ function Rect(x, y, width, height){
     }
 }
 
-function ObjCInt(int){
-    return int.intValue
-}
-
-function Number(int){
-    return NSNumber.numberWithInt(int)
-}
-
-function documentLoaded(){
-    function TodoieAppDelegate(){}
-  
-    function numberOfRows(section, a){
-        return ObjCInt(3)
-    }
-    
-    function numberOfSections(){
-        return ObjCInt(1)
-    }
-   
-    function tableViewCellForRowAtIndexPath(tableView, indexPath){
-        var cell = tableView.dequeueReusableCellWithIdentifier("FruitCell")
-        var fruit = this.fruits[Number(indexPath.row)]
-        var textLabel = cell.textLabel
-        cell.textLabel.text = fruit
-        cell.backgroundColor = UIColor.redColor
-        return cell
-    }
-    
-    function didFinish(a, b){
+function OnLoad(){
+    function DidFinishLaunching(application, options){
+        //Initialize the app delegate
         var appFrame = Rect(0, 0, 320, 480)
 
         var window = UIWindow.alloc.initWithFrameValue(appFrame)
         window.makeKeyAndVisible()
+        
+        // assigning a property with a value
+        // will invoke the setter on the receiver
         this.window = window
-
-        var tableView = UITableView.alloc.initWithFrameValue(appFrame)
-        tableView.registerClassForCellReuseIdentifier(UITableViewCell.class, "FruitCell")
-      
-        this.tableViewNumberOfRowsInSection = numberOfRows
-        this.numberOfSectionsInTableView = numberOfSections
-        this.tableViewCellForRowAtIndexPath = tableViewCellForRowAtIndexPath
-        window.addSubview(tableView)
+       
         this.fruits = ["apples", "banannas", "oranges"]
         
+        var tableView = UITableView.alloc.initWithFrameValue(appFrame)
+        tableView.registerClassForCellReuseIdentifier(UITableViewCell.class, "FruitCell")
         tableView.delegate = this
         tableView.dataSource = this
+        tableView.separatorColor = UIColor.clearColor
+        
+        var header = UILabel.alloc.initWithFrameValue(Rect(0, 0, 320, 50))
+        header.text = "Shopping list"
+        header.setTextAlignment(ObjCInt(1))
+        tableView.tableHeaderView = header
+        
+        this.tableView = tableView
+
+        window.addSubview(tableView)
     }
 
-    function TodoieAppDelegateInit(){
-        NSLog("Init!")
-        this.applicationDidFinishLaunchingWithOptions = didFinish
-        return this
+    function AppDelegate(){}
+
+    // Implementing a method can be achived by
+    // assigning a function to the property of a JSFunction or ObjC class
+    //
+    // it can also be achieved by assigning properties to 'this'
+    // best practice is to use prototypes or static initializers
+    // because assigning a function to a property causes the method
+    // to be implemented
+    
+    AppDelegate.prototype.applicationDidFinishLaunchingWithOptions = DidFinishLaunching
+
+    AppDelegate.prototype.dealloc = function(){
+        this.tableView.dataSource = null;
+        this.tableView.delegate = null;
+    }
+   
+    // UITableViewDataSource
+    AppDelegate.prototype.tableViewNumberOfRowsInSection = function(tableView, section){
+        return ObjCInt(3)
     }
     
-    TodoieAppDelegate.prototype.init = TodoieAppDelegateInit
+    AppDelegate.prototype.numberOfSectionsInTableView = function(tableView){
+        return ObjCInt(1)
+    }
+    
+    AppDelegate.prototype.tableViewCellForRowAtIndexPath = function(tableView, indexPath){
+        var cell = tableView.dequeueReusableCellWithIdentifier("FruitCell")
+        //A JS array is indexed by objects
+        var fruit = this.fruits[Number(indexPath.row)]
+        var textLabel = cell.textLabel
+        cell.textLabel.text = fruit
+        return cell
+    }
 }
 
-function main(a, b){
-    documentLoaded()
-    return UIApplication.applicationMainArgVName(a, b, "TodoieAppDelegate")
-}
+//The main function (invoked by the os)
+function main(argc, argv){
+    OnLoad()
 
+    //A wrapper for UIApplicationMain with a autorelease pool
+    return UIApplication.applicationMainArgVName(argc, argv, "AppDelegate")
+}
