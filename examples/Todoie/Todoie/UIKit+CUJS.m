355ea51b70694be47288552eb16578de3de5985a
Jerry Marino <i@jerrymarino.com>
2014-08-23 10:47:42 -0700

    Remove unused code from example

--- examples/Todoie/Todoie/UIKit+CUJS.m
+++ examples/Todoie/Todoie/UIKit+CUJS.m
@@ -18,43 +18,7 @@
 + (int)applicationMain:(int)argc argV:(char *[])argv name:(NSString *)appDelegateName
 {
     @autoreleasepool {
-        //FIXME : this stuff really belongs in some type of integration
-        // test and not the example - ignore this for now
-        CGRect rect = (CGRect){(double)10.0,10, 6, 6};
-
-        id structObject = (__bridge id)(objc_Struct(@"CGRect", &rect));
-        NSLog(@"%@", structObject);
-
-        CGRect retrievedRect;
-        NSValue *structValue = [structObject structValue];
-        [structValue getValue:&retrievedRect];
-        
-        NSLog(@"%@", structValue);
-        char *bytes = [structObject toStruct];
-
-        NSLog(@"%p", bytes);
-        CGRect retrievedRect2;
-        memcpy(&retrievedRect2, bytes, sizeof(CGRect));
-        free(bytes);
-        
-        CGPoint pt = (CGPoint){33,44};
-        id ptObject = (__bridge id)(objc_Struct(@"CGPoint", &pt));
-
-        CGPoint retrievedPoint;
-        NSValue *ptValue = [ptObject structValue];
-        [ptValue getValue:&retrievedPoint];
-        
-        NSString *rectEncode = [[structObject class] performSelector:NSSelectorFromString(@"structEncoding")];
-        NSString *ptEncode = [[ptObject class] performSelector:NSSelectorFromString(@"structEncoding")];
-
-        BOOL correctPtEncoding = [ptEncode isEqualToString:[NSString stringWithFormat:@"%s", @encode(CGPoint)]];
-        assert(correctPtEncoding);
-
-        BOOL correctRectEncoding = [rectEncode isEqualToString:[NSString stringWithFormat:@"%s", @encode(CGRect)]];
-        assert(correctRectEncoding);
-       
-        assert(!CGFLOAT_IS_DOUBLE);
-        UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        UIApplicationMain(argc, argv, nil, name);
         return 0;
     }
 }
