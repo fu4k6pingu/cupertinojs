c7e4fac4199a378dbc696e17845b749586a62858
Jerry Marino <i@jerrymarino.com>
2014-08-24 21:43:42 -0700

    Use v8 3.29.16

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,14 +3,14 @@ mkdir -p deps/
 # Install v8 with gyp (https://code.google.com/p/v8/wiki/BuildingWithGYP)
 git clone https://chromium.googlesource.com/external/v8.git deps/v8-trunk
 
+## Explicitly use Version 3.29.16 (based on bleeding_edge revision r23326)
+git checkout f284b29e37d97d7ee9128055862179dcbda7e398
+
 cd deps/v8-trunk/ 
 
 make builddeps
 
 build/gyp_v8 -Dtarget_arch=x64
-
-## If no gyp do this:
-# svn co http://gyp.googlecode.com/svn/trunk build/gyp --revision 1831
 
 cd ../..
 
